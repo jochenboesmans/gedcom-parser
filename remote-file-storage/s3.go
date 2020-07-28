8165d1dbfb362098674ae329930d7e06e81a1b0c
Jochen Boesmans <jboesmans@findmypast.com>
2020-07-28 23:49:34 +0100

    Add s3 write.

--- remote-file-storage/s3.go
+++ remote-file-storage/s3.go
@@ -1,4 +1,20 @@
 package remote_file_storage
 
-func bla() {
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	session2 "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"io"
+)
+
+func S3Write(r io.Reader) (*s3manager.UploadOutput, error) {
+	session := session2.Must(session2.NewSession())
+
+	uploader := s3manager.NewUploader(session)
+
+	return uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String("myBucketString"),
+		Key:    aws.String("myKeyString"),
+		Body:   r,
+	})
 }
