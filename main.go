8394825d970a168a09c3394f97c5eee85cf65740
Jochen Boesmans <jboesmans@findmypast.com>
2020-07-31 21:56:15 +0100

    Handle unpassed arguments.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/jochenboesmans/gedcom-parser/grpc"
 	"github.com/jochenboesmans/gedcom-parser/parse"
+	"log"
 	"os"
 )
 
@@ -10,8 +11,14 @@ import (
 //var outputFilePath = flag.String("output", "", "path to output file")
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("please choose a command (parse|serve)")
+	}
 	switch os.Args[1] {
 	case "parse":
+		if len(os.Args) < 4 {
+			log.Fatalln("please supply inputFilePath and outputFilePath respectively")
+		}
 		parse.Parse(os.Args[2], os.Args[3])
 	case "serve":
 		grpc.Serve()
