506e192dc1915fa0bb4e14aa7ffdc7fe8545b0ab
Jochen Boesmans <jboesmans@findmypast.com>
2020-07-31 21:52:50 +0100

    Use 7z instead of gz

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,14 +1,15 @@
-# clean
-rm gedcom-parser.{darwin,linux,windows}.amd64.tar.gz
+# clean previous zip
+rm gedcom-parser.{darwin,linux,windows}.7z
 
 # build
 env GOOS=darwin GOARCH=amd64 go build -o gedcom-parser.darwin.amd64
 env GOOS=linux GOARCH=amd64 go build -o gedcom-parser.linux.amd64
 env GOOS=windows GOARCH=amd64 go build -o gedcom-parser.windows.amd64
 
-# tar
-tar -czf gedcom-parser.darwin.amd64.tar.gz gedcom-parser.darwin.amd64
-tar -czf gedcom-parser.linux.amd64.tar.gz gedcom-parser.linux.amd64
-tar -czf gedcom-parser.windows.amd64.tar.gz gedcom-parser.windows.amd64
+# zip
+7z a gedcom-parser.darwin.amd64.7z gedcom-parser.darwin.amd64
+7z a gedcom-parser.linux.amd64.7z gedcom-parser.linux.amd64
+7z a gedcom-parser.windows.amd64.7z gedcom-parser.windows.amd64
 
+# clean build
 rm gedcom-parser.{darwin,linux,windows}.amd64
