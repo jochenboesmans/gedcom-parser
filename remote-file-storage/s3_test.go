db925e3fb4885a1a0fd0cdc03efd8e7d3f31cde7
Jochen Boesmans <jboesmans@findmypast.com>
2020-08-01 22:42:19 +0100

    Update S3 tests.

--- remote-file-storage/s3_test.go
+++ remote-file-storage/s3_test.go
@@ -1,12 +1,42 @@
 package remote_file_storage
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/joho/godotenv"
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
+type server struct {
+	downloader *s3manager.Downloader
+	uploader   *s3manager.Uploader
+}
+
+var s server
+
+func (s *server) Init() {
+	sess := session.Must(session.NewSession(aws.NewConfig().WithRegion(os.Getenv("AWS_REGION"))))
+
+	s.uploader = s3manager.NewUploader(sess)
+	s.downloader = s3manager.NewDownloader(sess)
+}
+
+func setup() {
+	_ = godotenv.Load("../.env")
+	s.Init()
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	os.Exit(code)
+}
+
 func TestS3Read(t *testing.T) {
-	_, err := S3Read("ITIS.ged")
+	_, err := S3Read("gedcom/ITIS.ged", s.downloader)
 	if err != nil {
 		t.Errorf("failed to read from s3: %s", err)
 	}
@@ -18,7 +48,7 @@ func TestS3Write(t *testing.T) {
 		t.Errorf("failed to open file: %s", err)
 	}
 
-	_, err = S3Write("gedcom/ITIS.ged", &f)
+	_, err = S3Write("gedcom/ITIS.ged", &f, s.uploader)
 	if err != nil {
 		t.Errorf("failed to write to s3: %s", err)
 	}
