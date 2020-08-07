51a542b103a15a0d1c5b0bf7a013120836a43774
Jochen Boesmans <jboesmans@findmypast.com>
2020-08-07 19:22:24 +0100

    Improve logging, help on usage.

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func init() {
 	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found. Some functionality might not work.")
+		log.Println("No .env file found. Some functionality might not work if you don't supply environment variables.")
 	}
 }
 
@@ -22,19 +22,34 @@ func main() {
 		parse.Parse(os.Args[2], os.Args[3])
 	case "serve":
 		grpc.Serve()
+	case "help":
+		helpMessage := `
+		Usage: 'gedcom-parser <command> <inputFilePath> <outputFilePath>'
+
+		* <command> [REQUIRED]:
+			parse - Parse local files. Requires the inputFilePath and outputFilePath to be specified.
+			serve - Start a gRPC server for gedcom parsing on remote file storage.
+
+		* <inputFilePath> [OPTIONAL]:
+			Relative path to the input file to parse. Please make sure to use the file extensions .ged, .json and .protobuf for respectively GEDCOM, JSON and Protobuf files.
+			
+		* <outputFilePath> [OPTIONAL]:
+			Relative path to the output file. Please make sure to use the file extensions .ged, .json and .protobuf for respectively GEDCOM, JSON and Protobuf files.
+		`
+		log.Fatal(helpMessage)
 	default:
-		log.Fatalln("please choose a command in (parse|serve)")
+		log.Fatalln("please choose a valid command (use 'gedcom-parser help' for more information on usage)")
 	}
 }
 
 func checkMainArg() {
 	if len(os.Args) < 2 {
-		log.Fatalln("please choose a command (parse|serve)")
+		log.Fatalln("please choose a command (use 'gedcom-parser help' for more information on usage)")
 	}
 }
 
 func checkFilepathArgs() {
 	if len(os.Args) < 4 {
-		log.Fatalln("please supply inputFilePath and outputFilePath respectively")
+		log.Fatalln("please supply inputFilePath and outputFilePath respectively (use 'gedcom-parser help' for more information on usage)")
 	}
 }
