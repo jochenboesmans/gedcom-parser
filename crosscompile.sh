da46b9d7a4b75051606ccfcca60f6140f7dfb1b9
Jochen Boesmans <jochen.boesmans@gmail.com>
2021-01-09 21:46:22 +0000

    Include darwin/ARM64 in crosscompile.sh to support new Mac M1s

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,15 +1,17 @@
 # clean previous zip
-rm gedcom-parser.{darwin,linux,windows}.7z
+rm gedcom-parser.{darwin,linux,windows}.{amd64,arm64}.7z
 
 # build
 env GOOS=darwin GOARCH=amd64 go build -o gedcom-parser.darwin.amd64
+env GOOS=darwin GOARCH=arm64 go build -o gedcom-parser.darwin.arm64
 env GOOS=linux GOARCH=amd64 go build -o gedcom-parser.linux.amd64
 env GOOS=windows GOARCH=amd64 go build -o gedcom-parser.windows.amd64
 
 # zip
 7z a gedcom-parser.darwin.amd64.7z gedcom-parser.darwin.amd64
+7z a gedcom-parser.darwin.arm64.7z gedcom-parser.darwin.arm64
 7z a gedcom-parser.linux.amd64.7z gedcom-parser.linux.amd64
 7z a gedcom-parser.windows.amd64.7z gedcom-parser.windows.amd64
 
 # clean build
-rm gedcom-parser.{darwin,linux,windows}.amd64
+rm gedcom-parser.{darwin,linux,windows}.{amd64,arm64}
