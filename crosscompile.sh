1a472da489295fdc77fe5e6ca91270cb192c5001
Jochen Boesmans <jochen.boesmans@gmail.com>
2021-03-11 23:09:18 +0000

    Fix bug in crosscompile.sh

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 declare -a os=("darwin" "darwin" "linux" "windows")
 declare -a arch=("amd64" "arm64" "amd64" "amd64")
 
-for i in "${os[@]}"; do
+for ((i=0; i<${#os[*]}; i++)); do
   env GOOS="${os[$i]}" GOARCH="${arch[$i]}" go build -o gedcom-parser."${os[$i]}"."${arch[$i]}"
   7z a gedcom-parser."${os[$i]}"."${arch[$i]}".7z gedcom-parser."${os[$i]}"."${arch[$i]}"
+  echo "${i}"
 done
